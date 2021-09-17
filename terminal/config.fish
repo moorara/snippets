3d5bb6162cb076ad68e2220003e0fb576e771c22
Milad <moorara@users.noreply.github.com>
2021-09-17 15:17:00 -0400

    Create config.fish

---
+++ terminal/config.fish
@@ -0,0 +1,5 @@
+# bobthefish theme configurations
+set -g theme_display_user          no
+set -g theme_display_k8s_context   yes
+set -g theme_display_k8s_namespace yes
+set -g theme_show_exit_status      yes
