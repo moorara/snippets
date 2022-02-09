821085aeb976edd21b45404375542e3666a9cb47
Milad <moorara@users.noreply.github.com>
2022-02-09 15:19:44 -0500

    Update config.fish

--- terminal/config.fish
+++ terminal/config.fish
@@ -7,3 +7,6 @@ set -g theme_display_user no
 set -g theme_display_k8s_context yes
 set -g theme_display_k8s_namespace yes
 set -g theme_show_exit_status yes
+
+# Add Go bin path to global PATH variable
+set -x PATH $PATH $HOME/go/bin
